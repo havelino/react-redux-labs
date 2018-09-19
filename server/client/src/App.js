6f2aa4a7153e0b7c6b685a511c46dfb1d0f19e30
Hugo Alberto Avelino Hernandez <hha0009@email.wal-mart.com>
2018-09-19 14:55:29 -0500

    adding boostrap to the project

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ThemeSwitcher from './theme-switcher'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm">
+              <a href="#">Home</a>
+              </div>
+              <div class="col-sm">
+              <a href="#">About</a>
+              </div>
+              <div class="col-sm">
+              <a href="#">Login</a>
+              </div>
+              <div class="col-sm">
+              <a href="#">Checkout</a>
+              </div>
+            </div>
+          </div>
+          <h1 className="App-title">Dinner and A Movie</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ThemeSwitcher></ThemeSwitcher>
       </div>
     );
   }
