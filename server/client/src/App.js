376c7e8a6d1ed9d539a40e8fada03dd7a3452a72
Hugo Alberto Avelino Hernandez <hha0009@email.wal-mart.com>
2018-09-19 16:23:18 -0500

    setup components

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ThemeSwitcher from './theme-switcher'
+import ThemeSwitcher from './components/ThemeSwitcher'
 
 class App extends Component {
   render() {
